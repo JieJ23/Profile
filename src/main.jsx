b5927dea0def1fc23512d11fde62abde45d60dda
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-04-02 14:54:49 -0400

    Fixed Router Setting

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Projects from "./Pages/Projects";
 import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/Projects",
-    element: <Projects />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Projects" element={<Projects />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
